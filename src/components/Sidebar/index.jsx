ea3b9faeb1214acebb2b3d2e91d46dcd7d2c5b2d
sikaco <sikaco0@gmail.com>
2017-06-05 14:49:12 +0800

    localStore disassembled and best practice added

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,26 +1,48 @@
 import React, { Component } from 'react'
+import { observable, action, computed } from 'mobx'
 import { observer } from 'mobx-react'
-import { Tabs } from 'antd'
+import { Radio } from 'antd'
 import './index.less'
 
-const TabPane = Tabs.TabPane
+// import Collection from 'Collection'
+
+const tabContentMap = {
+  0: <div>test1</div>,
+  1: <div>test2</div>
+}
+
+class LocalStore {
+  @observable tabIndex = 0
+
+  @action changeTab(index) {
+    this.tabIndex = index
+  }
+
+  @computed get tabContent() {
+    return tabContentMap[this.tabIndex]
+  }
+}
 
 @observer
 export default class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.store = new LocalStore()
+  }
+
+  handleTabChange = (e) => {
+    this.store.changeTab(e.target.value)
+  }
+
   render() {
     return (
       <div id="sidebar">
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Tab 1" key="1">
-            sidebar
-            {/*<Collection key="recent"/>
-             <Collection key="trash"/>
-             <Notebooks/>*/}
-          </TabPane>
-          <TabPane tab="Tab 2" key="2">
-
-          </TabPane>
-        </Tabs>
+        <Radio.Group value={this.store.tabIndex} onChange={this.handleTabChange}>
+          <Radio.Button value={0}>Notebooks</Radio.Button>
+          <Radio.Button value={1}>Tags</Radio.Button>
+        </Radio.Group>
+        {this.store.tabContent}
       </div>
     )
   }
