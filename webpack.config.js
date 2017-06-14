2fbfd7474131140502a5836d6768738c68f3aee5
sikaco <sikaco0@gmail.com>
2017-06-14 17:17:19 +0800

    Code splitted of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const {resolve} = require('path')
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const Merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const commonCfg = {
+  entry: {
+    vendor: [
+      'mobx',
+      'mobx-react',
+      'mobx-react-devtools',
+      'react',
+      'react-dom',
+      'react-hot-loader'
+    ]
+  },
   context: resolve(__dirname, 'src'),
   output: {
-    filename: 'bundle.js',	// 输出的打包文件
     path: resolve(__dirname, 'dist'),
     publicPath: '/'	// 对于热替换(HMR)是必须的，让 webpack 知道在哪里载入热更新的模块(chunk)
   },
@@ -28,13 +38,18 @@ const commonCfg = {
 }
 
 const devCfg = {
-  entry: [
-    'react-hot-loader/patch',	// 开启 React 代码的模块热替换(HMR)
-    'webpack-dev-server/client?http://localhost:8080',	// 为 webpack-dev-server 的环境打包代码, 然后连接到指定服务器域名与端口
-    'webpack/hot/only-dev-server',	// 为热替换(HMR)打包好代码	// only- 意味着只有成功更新运行代码才会执行热替换(HMR)
+  entry: {
+    main: [
+      'react-hot-loader/patch',	// 开启 React 代码的模块热替换(HMR)
+      'webpack-dev-server/client?http://localhost:8080',	// 为 webpack-dev-server 的环境打包代码, 然后连接到指定服务器域名与端口
+      'webpack/hot/only-dev-server',	// 为热替换(HMR)打包好代码	// only- 意味着只有成功更新运行代码才会执行热替换(HMR)
 
-    './index.tsx'	// 我们 app 的入口文件
-  ],
+      './index.tsx'	// 我们 app 的入口文件
+    ],
+  },
+  output: {
+    filename: '[name].js',	// 输出的打包文件
+  },
   devtool: 'inline-source-map',
   devServer: {
     hot: true,	// 开启服务器的模块热替换(HMR)
@@ -42,18 +57,45 @@ const devCfg = {
     publicPath: '/'	// 和上文 output 的“publicPath”值保持一致
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: [
+        'vendor', // 指定公共 bundle 的名字。
+        'manifest'
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'demo',
+      template: resolve(__dirname, 'dist') + '/index.html' // 模板路径
+    }),
     new webpack.HotModuleReplacementPlugin(),	// 开启全局的模块热替换(HMR)
     new webpack.NamedModulesPlugin(),	// 当模块热替换(HMR)时在浏览器控制台输出对用户更友好的模块名字信息
   ],
 }
 
 const prodCfg = {
-  entry: [
-    './index.tsx'	// 我们 app 的入口文件
-  ],
+  entry: {
+    main: [
+      './index.tsx'	// 我们 app 的入口文件
+    ]
+  },
+  output: {
+    filename: '[name].[chunkHash:6].js',	// 输出的打包文件
+  },
   devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: [
+        'vendor', // 指定公共 bundle 的名字。
+        'manifest'
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'demo',
+      template: resolve(__dirname, 'dist') + '/index.html' // 模板路径
+    })
+  ]
 }
 
 module.exports = function (env) {
-  return env === 'prod' ? merge(commonCfg, prodCfg) : merge(commonCfg, devCfg)
+  return env === 'prod' ? Merge(commonCfg, prodCfg) : Merge(commonCfg, devCfg)
 }
